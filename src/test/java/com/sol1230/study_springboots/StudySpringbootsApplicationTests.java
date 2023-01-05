26ca5132cc35c9e11960f9f952719a0148f0bc88
sol1230 <solllll1230@gmail.com>
2023-01-05 12:12:45 +0900

    first commit

---
+++ src/test/java/com/sol1230/study_springboots/StudySpringbootsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sol1230.study_springboots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringbootsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
